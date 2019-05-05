be9669f8551728bc7a970c3d4efe039237ea3d56
diegocardozo97 <diego_a_cardozo@hotmail.com>
2019-05-05 18:50:40 -0500

    Add methods to convert between float and short array.

---
+++ catkin_home/src/devices/src/RnnoiseMethods/HelperMethods.hpp
@@ -0,0 +1,18 @@
+#ifndef RNNOISE_METHODS__HELPER_METHODS_HPP
+#define RNNOISE_METHODS__HELPER_METHODS_HPP
+
+
+void convertFromShortArrayToFloatArray(uint16_t *in_short_array, const long num_elements, float *out_float_array) {
+  for (long i = 0; i < num_elements; ++i) {
+    out_float_array[i] = in_short_array[i];
+  }
+}
+
+void convertFromFloatArrayToShortArray(float *in_float_array, const long num_elements, uint16_t *out_short_array) {
+  for (long i = 0; i < num_elements; ++i) {
+    out_short_array[i] = in_float_array[i];
+  }
+}
+
+
+#endif
