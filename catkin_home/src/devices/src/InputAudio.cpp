9453d8508dbc4fe282cc1cdc450e6ee228693e51
diegocardozo97 <diego_a_cardozo@hotmail.com>
2019-05-05 19:06:43 -0500

    Add rnnoise's state init and warm up.

--- catkin_home/src/devices/src/InputAudio.cpp
+++ catkin_home/src/devices/src/InputAudio.cpp
@@ -1,5 +1,14 @@
+#include <cstdio>
+
+#include <iostream>
+
 #include "ros/ros.h"
 #include "audio_common_msgs/AudioData.h"
+extern "C" {
+#include "rnnoise.h"
+}
+
+#include "RnnoiseMethods/HelperMethods.hpp"
 
 
 using namespace std;
@@ -11,6 +20,28 @@ constexpr int NUMBER_FRAMES_RNNOISE = 480L;
 
 ros::Publisher publi;
 
+DenoiseState *st;
+
+
+void RNNoiseWarmUp(DenoiseState *st) {
+  int i;
+  float x[NUMBER_FRAMES_RNNOISE];
+  
+  FILE *f1;
+  f1 = fopen("./warmup_street_20dB.raw", "r");
+
+  while (true) {
+    short tmp[NUMBER_FRAMES_RNNOISE];
+    fread(tmp, sizeof(short), NUMBER_FRAMES_RNNOISE, f1);
+    if (feof(f1)) break;
+
+    convertFromShortArrayToFloatArray(tmp, NUMBER_FRAMES_RNNOISE, x);
+
+    rnnoise_process_frame(st, x, x);
+  }
+  
+  fclose(f1);
+}
 
 void rnnoise_process_new_input(uint16_t new_input[NUMBER_FRAMES_RNNOISE]) {}
 
@@ -37,6 +68,10 @@ void onAudioCallback(const audio_common_msgs::AudioData::ConstPtr msg){
 int main(int argc, char **argv){
     ros::init(argc,argv,"InputAudio");
     
+    st = rnnoise_create();
+    RNNoiseWarmUp(st);
+    std::cout << "DenoiseState ready.\n";
+
     
     ros::NodeHandle n;
     publi = n.advertise<audio_common_msgs::AudioData>("UsefulAudio", BUFFER_SIZE);
