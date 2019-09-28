a246e958247ad799d8c904d4ceab4b6da0822ddf
Paul Vazquez <pev@live.com.mx>
2019-09-28 16:57:12 -0500

    rasa parser created

--- catkin_home/src/action_selectors/scripts/parser.py
+++ catkin_home/src/action_selectors/scripts/parser.py
@@ -3,6 +3,23 @@ import rospy
 from action_selectors.msg import RawInput
 from intercom.msg import action_selector_cmd
 
+'''
+Voz: repetir de vuelta un comando de voz con el formato "Bring [OBJETO] from [LUGAR]."
+Punto Inicial: enmedio de la arena.
+Calificación Base: 5 si entiende el objeto, 5 si entiende el lugar.
+Reto Adicional: entender ambos el objeto y el lugar por medio de utilizar algún esquema de Interacción Humano-Robot donde el robot 
+tiene la iniciativa de la conversación; 2.5 puntos adicionales por la correcta comprensión de ambos.
+
+El robot comienza en el punto inicial, donde recibe el comando por voz en el formato "Bring [OBJETO] frome [LUGAR]."
+Se puede utilizar otro esquema, pero no se darán puntos adicionales.
+El robot navega al lugar pedido.
+El robot agarra el objeto pedido.
+
+
+'''
+
+
+
 def callback(msg):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg.inputText)
 
@@ -10,6 +27,8 @@ def callback(msg):
     #call parser
     #send message to engine 
     pub = rospy.Publisher('action_selector_cmds', action_selector_cmd, queue_size=10)
+    #Here the parsing is done
+
     action_code = action_selector_cmd()
     action_code.cmd_id = 1
     action_code.cmd_priority = 1
