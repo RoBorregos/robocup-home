c8256b40e32256e80d14960c8628897cecfc8ea6
Diego Cardozo <diego_a_cardozo@hotmail.com>
2020-01-29 11:01:31 -0600

    Modify action_selectors/hear.py to call DeepSpeech
    Modify hear.py to subscribe to UsefulAudio topic, call DeepSpeech/deploy/ros_server.ASRServer that calls DeepSpeech2, and publish the text to RawInput topic. With this commit, all the stack since capturing sound, looking for voice, converting speech to text, and publishing the voice is ready.

--- catkin_home/src/action_selectors/scripts/hear.py
+++ catkin_home/src/action_selectors/scripts/hear.py
@@ -2,43 +2,51 @@
 import rospy
 from audio_common_msgs.msg import AudioData
 from action_selectors.msg import RawInput
-import numpy as np
-import matplotlib.pyplot as plt
+# TODO: Analyze why inside the DeepSpeech dir, everything can be imported
+# directly without the package. Maybe is because the _init_paths.py and/or
+# the `absolute_import`.
+from DeepSpeech.deploy.ros_server import ASRServer
+
+
+publisher = None
+asr_server = None
 
 
 def callback(data):
-    print("LISTENED")
-    print("DATA DATA", type(data))
-    print(data.data)
-    arr = [(ord(i)) for i in data.data]
-    print(arr)
-    print("PLAYED")
-    pub = rospy.Publisher('RawInput', RawInput, queue_size=10)
-    isWoman=False
-    #Subscribe to audio_commons to get audio from microphone
-    ##Cardozo code here --------------
-    ##Audio 2 text
+    rospy.loginfo("Received a voice audio, computing...")
+    
+    unicode_text = asr_server.bytes_speech_to_text(data.data)
+    text = unicode_text.encode('ascii', 'ignore')
+    rospy.loginfo("Voice audio said: \"{0}\".".format(text))
+
     msg = RawInput()
-    msg.isWoman =isWoman
-    msg.inputText = "RandomTExt %s" % rospy.get_time() 
-    ##------------------
-    rospy.loginfo(msg)
-    pub.publish(msg)
-    isWoman=not isWoman
+    # TODO: Check this isWoman field.
+    msg.isWoman = False
+    msg.inputText = text
+    publisher.publish(msg)
+    rospy.loginfo("Published the msg.")
 
-def listener():
 
+def main():
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
     rospy.init_node('hear', anonymous=True)
+    rospy.loginfo("*Node initiated, starting ASRServer (DS2)*")
 
-    rospy.Subscriber("UsefulAudio", AudioData, callback)
+    global asr_server
+    asr_server = ASRServer()
+    rospy.loginfo("*ASRServer ready.*")
+
+    global publisher
+    publisher = rospy.Publisher('RawInput', RawInput, queue_size=10)
+    rospy.Subscriber("UsefulAudio", AudioData, callback, queue_size=10)
 
     # spin() simply keeps python from exiting until this node is stopped
+    rospy.loginfo("*Ready to callback.*")
     rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    main()
