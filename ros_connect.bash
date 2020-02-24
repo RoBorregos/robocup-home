a9f52499ab893cb04b7f431ea6554b873fc107a8
Paul Vazquez <pev@live.com.mx>
2020-02-24 13:28:51 -0800

    Added case when both ips are provided

--- ros_connect.bash
+++ ros_connect.bash
@@ -1,6 +1,14 @@
 #!/bin/bash
-echo "Be sure to call this script: source ./ros_connect {ROS_MASTER_IP}"
-if [ $# == 1 ] 
+echo "Be sure to call this script: source ./ros_connect {ROS_MASTER_IP} or source ./ros_connect {ROS_MASTER_IP} {ROS_TALKER_IP}"
+echo $#
+if [ $# == 2 ]
+then 
+    #Both ips provided
+    ROS_MASTER_URI="http://$1:11311"
+    ROS_IP=$2 
+    export ROS_MASTER_URI
+    export ROS_IP
+elif [ $# == 1 ] 
 then 
     my_ip=`hostname -I |  awk '{print $1;}'` #Assumes the first ip returned is the one needed
     echo "Your IP: " $my_ip
@@ -10,6 +18,7 @@ then
     export ROS_MASTER_URI
     export ROS_IP
     echo "Done setting ROS_MASTER_URI and ROS_IP"
+
 else
     echo "No IP address of master ROS computer provided"
 fi
