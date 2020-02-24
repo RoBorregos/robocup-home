5c68f34276301c042bf7cb39f75d03a5073682c6
Paul Vazquez <pev@live.com.mx>
2020-02-24 13:23:02 -0800

    Added script to connect computer to ROS_MASTER running in another device

--- ros_connect.bash
+++ ros_connect.bash
@@ -1,8 +1,15 @@
 #!/bin/bash
-if [ $# -lt '3' ]; then 
-    echo $#
-    my_ip=` hostname -I `
-    echo $my_ip
+echo "Be sure to call this script: source ./ros_connect {ROS_MASTER_IP}"
+if [ $# == 1 ] 
+then 
+    my_ip=`hostname -I |  awk '{print $1;}'` #Assumes the first ip returned is the one needed
+    echo "Your IP: " $my_ip
+    echo "ROS MASTER IP: " $1
+    ROS_MASTER_URI="http://$1:11311"
+    ROS_IP=$my_ip 
+    export ROS_MASTER_URI
+    export ROS_IP
+    echo "Done setting ROS_MASTER_URI and ROS_IP"
 else
-echo "No IP address of host computer provided"
+    echo "No IP address of master ROS computer provided"
 fi
