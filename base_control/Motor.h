7465527ac42438f77f22f336f4661bb9ebf7ffd4
Josecisneros001 <joseacisnerosm@gmail.com>
2020-02-26 18:15:09 -0600

    PID integration in motor

--- base_control/Motor.h
+++ base_control/Motor.h
@@ -3,23 +3,39 @@
 #define MotorClass
 class Motor {
   public:
-    //PWM value
-    int pwm = 0;
-    //PWM PINS
-    byte m1 = 0;
-    byte m2 = 0;
-    //STATE
-    int state = 0;
+    byte id=0;
 
-    //
-    Motor(byte a, byte b);
+    byte pwm = 0;
+    double ticks=0;
+
+    //Pins
+    byte d1 = 0;
+    byte d2 = 0;
+    byte p1 = 0;
+
+    byte e1 = 0;
+    byte e2 = 0;
+
+    //STATE
+    byte state = 0;
+
+    //PID
+    double Kp = 5.2, Ki = 0, Kd = 0;
+    PID _PID;
+
+    Motor(byte id,byte d1, byte d2, byte p1, byte e1,byte e2);
     Motor();
     void Forward();
     void Backward();
     void Stop();
     void defineOutput();
     void changePWM(byte p);
+    int getTicks();
+    void setTicks(int ticks);
+    void _IPID(double actual,double target);
+    double _OPID();
+    
 };
-Motor
+
 
 #endif
---
+++ base_control/Motor.ino
@@ -0,0 +1,90 @@
+Motor::Motor() {}
+
+
+Motor::Motor(byte id,byte d1, byte d2, byte p1, byte e1,byte e2) : _PID(0,0,0, Kp, Ki, Kd, DIRECT) {
+  this->id = id;
+  this->d1 = d1;
+  this->d2 = d2;
+  this->p1 = p1;
+  this->e1 = e1;
+  this->e2 = e2;
+  this->state=3;
+  this->defineOutput();
+  this->changePWM(0);
+
+  //PID
+  _PID.SetParams(0,0,0);
+  _PID.SetOutputLimits(-50, 50);
+  _PID.SetTunings(Kp, Ki, Kd);
+  _PID.SetSampleTime(1);
+  _PID.SetMode(AUTOMATIC);
+}
+
+void Motor::_IPID(double actual,double target){
+  _PID.SetInputs(actual, target);
+}
+double Motor::_OPID(){
+  return _PID.GetOutput();
+}
+void Motor::defineOutput() {
+  pinMode(this->d1, OUTPUT);
+  pinMode(this->d2, OUTPUT);
+  pinMode(this->p1, OUTPUT);
+  pinMode(this->e1, INPUT);
+  pinMode(this->e2, INPUT);
+  
+  switch (this->id){
+    case 1:
+      attachInterrupt(digitalPinToInterrupt(this->e1), BLencoder, RISING);
+    break;
+    case 2:
+      attachInterrupt(digitalPinToInterrupt(this->e1), FLencoder, RISING);
+    break;
+    case 3:
+      attachInterrupt(digitalPinToInterrupt(this->e1), BRencoder, RISING);
+    break;
+    case 4:
+      attachInterrupt(digitalPinToInterrupt(this->e1), FRencoder, RISING);
+    break;
+  }
+}
+int Motor::getTicks() {
+  return this->ticks;
+}
+void Motor::setTicks(int ticks) {
+  this->ticks=ticks;
+}
+
+void Motor::changePWM(byte p){
+  this->pwm=p;
+  switch(state){
+    case 1:
+      this->Forward();
+    break;
+    case 2:
+      this->Backward();
+    break;
+    case 3:
+      this->Stop();
+    break;
+  }
+}
+
+void Motor::Stop() {
+  analogWrite(this->p1, 0);
+  digitalWrite(this->d1, 0);
+  digitalWrite(this->d2, 0);
+  this->state=3;
+}
+void Motor::Forward() {
+  analogWrite(this->p1, this->pwm);
+  digitalWrite(this->d1, 1);
+  digitalWrite(this->d2, 0);
+  this->state=1;
+}
+void Motor::Backward() {
+  analogWrite(this->p1, this->pwm);
+  digitalWrite(this->d1, 0);
+  digitalWrite(this->d2, 1);
+  this->state=2;
+}
