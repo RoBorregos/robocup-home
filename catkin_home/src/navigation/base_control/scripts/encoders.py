78b001af3a9b1a0638d461d68c77fd8aecb531cc
Josecisneros001 <joseacisnerosm@gmail.com>
2020-09-25 19:25:39 -0500

    add to simulation of encoders all directions

--- catkin_home/src/navigation/base_control/scripts/encoders.py
+++ catkin_home/src/navigation/base_control/scripts/encoders.py
@@ -13,35 +13,42 @@ def move():
     rospy.loginfo("Node Initiated")
     
     #Initialize Twist Message
-    enc_msg = StampedEncoders()
-    enc_msg.encoders.left_wheel = 10000
-    enc_msg.encoders.right_wheel = 10000
-    enc_msg.encoders.time_delta = 0.005
+    enc_msgFront = StampedEncoders()
+    enc_msgBack  = StampedEncoders()
+    enc_msgFront.encoders.time_delta = 0.005
+    enc_msgBack.encoders.time_delta = 0.005
 
     forward  = 10000
     backward = -10000
     stop     = 0 
-    #Direction:      0     , 45   , 90   , 135   , 180   , 225   , 270   , 315     Left   Right   
-    encFrontL  =    [forward,forward,forward,stop,backward,backward,backward,stop,backward,forward]
-    encBackL   =    [backward,stop,forward,forward,forward,stop,backward,backward,backward,forward]
-    encFrontR  =    [backward,stop,forward,forward,forward,stop,backward,backward,forward,backward]
-    encBackR   =    [forward,forward,forward,stop,backward,backward,backward,stop,forward,backward]
+   
+    #Direction:      0       ,180     ,90     ,270     ,45     ,225     ,135    ,315     ,Left    ,Right   
+    encFrontL  =    [forward ,backward,forward,backward,forward,backward,stop   ,stop    ,backward,forward ]
+    encBackL   =    [backward,forward ,forward,backward,stop   ,stop    ,forward,backward,backward,forward ]
+    encFrontR  =    [backward,forward ,forward,backward,stop   ,stop    ,forward,backward,forward ,backward]
+    encBackR   =    [forward ,backward,forward,backward,forward,backward,stop   ,stop    ,forward ,backward]
     
     t0 = rospy.Time.now().to_sec()
     t1 = rospy.Time.now().to_sec()
-
+    indexDirection = 0
     while not rospy.is_shutdown():
     
         if rospy.Time.now().to_sec() - t0 > 5:
             t0 = rospy.Time.now().to_sec()
             
-            front_publisher.publish(enc_msg)
-            back_publisher.publish(enc_msg)
+            front_publisher.publish(enc_msgFront)
+            back_publisher.publish(enc_msgBack)
         
         if rospy.Time.now().to_sec() - t1 > 5:
             t1 = rospy.Time.now().to_sec()
-            enc_msg.encoders.left_wheel *= -1
-            enc_msg.encoders.right_wheel *= -1
+            indexDirection = indexDirection + 1
+            if indexDirection > 9:
+                indexDirection = 0
+            
+        enc_msgFront.encoders.left_wheel  = encFrontL[indexDirection]
+        enc_msgBack.encoders.left_wheel   = encBackL[indexDirection]
+        enc_msgFront.encoders.right_wheel = encFrontR[indexDirection]
+        enc_msgBack.encoders.right_wheel  = encBackR[indexDirection]
 
         #Publish Rate
         rate.sleep()
