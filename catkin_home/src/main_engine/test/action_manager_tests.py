516ee02a298db21a7db4149ec6fcfecb286934e2
Paul Vazquez <pev@live.com.mx>
2020-10-11 18:27:38 -0700

    First unit test

--- catkin_home/src/main_engine/test/action_manager_tests.py
+++ catkin_home/src/main_engine/test/action_manager_tests.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
-PKG='main_engine'
-
-import sys
-import unittest
-import os
 from main_engine.action_manager import Main_Engine
+import os
+import unittest
+import sys
+PKG = 'main_engine'
 
-## Testing the Main engine module of the robot
+
+# Testing the Main engine module of the robot
+
 class TestActionManager(unittest.TestCase):
     def setUp(self):
         self.action_manager = Main_Engine()
 
-
     def test_initial_values(self):
         self.assertEquals(self.action_manager.action_queue, [], "Not empty")
 
+
 if __name__ == '__main__':
     import rosunit
-    
     rosrunit.unitrun(PKG, 'test_action_manager', TestActionManager)
