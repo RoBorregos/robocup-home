291b72f481d4fc1c860b0160050a12ce9482cb1b
AYM1607 <u.g.a.mariano@gmail.com>
2021-05-09 23:19:16 -0500

    Adds full duplex communication between the ROS node and flask server.

--- catkin_home/src/hri/scripts/flask_server.py
+++ catkin_home/src/hri/scripts/flask_server.py
@@ -1,7 +1,7 @@
 import flask
 import eventlet
 import cv2
-from multiprocessing.connection import Listener
+from multiprocessing.connection import Listener, Client
 
 from flask import request, jsonify
 from flask_socketio import SocketIO, send
@@ -13,18 +13,46 @@ CORS(app)
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Establish connection with the HRI ROS node process.
-address = ('localhost', 7000)
-ros_listener = Listener(address)
-ros_connection = ros_listener.accept()
+# Full duplex connection with the HRI ros node.
 
+receiver_address = ('localhost', 7000)
+sender_address = ('localhost', 7001)
+
+ros_receiver_listener = None
+ros_receiver = None
+ros_sender = None
+
+def initialize_ros_sender():
+    # Wait to give the ros node time to call its accept method.
+    socketio.sleep(1)
+    ros_sender = Client(sender_address)
+
+def initialize_ros_receiver():
+    ros_receiver_listener = Listener(receiver_address)
+    ros_receiver = ros_receiver_listener.accept()
+
+def cleanup():
+    ros_sender.close()
+
+    
 # Forward all messages to "Comm" websocket endpoint.
-def message_forwarder():
+def ros_receive_handler():
     while True:
+        if ros_receiver is None:
+            # To avoid blocking the process
+            socketio.sleep(0.01)
+            continue
         # Only call `recv` when we're sure there's a new message since
         # it is a blocking call.
-        if(ros_connection.poll()):
-            socketio.emit("Comm", ros_connection.recv())
+        if(ros_receiver.poll()):
+            message = ros_receiver.recv()
+            if message == "CreateSender":
+                initialize_ros_sender()
+            elif message == "Close":
+                cleanup()
+            else:
+                # TODO: this should be serialized/processed
+                socketio.emit("Comm", message)
         socketio.sleep(0.1)
 
 @socketio.on("connect")
@@ -53,7 +81,10 @@ if __name__ == "__main__":
     # Patch python to make use of an event loop and make background tasks work.
     eventlet.monkey_patch()
 
-    socketio.start_background_task(message_forwarder)
+    initialize_ros_receiver()
+    
+    socketio.start_background_task(ros_receive_handler)
     
     # Do not use default port 5000, it might be used by ROS or other os service.
     socketio.run(app, use_reloader=False, port=5050)
+
--- catkin_home/src/hri/scripts/main.py
+++ catkin_home/src/hri/scripts/main.py
@@ -3,29 +3,60 @@ import rospy
 import os
 import subprocess as sp
 
-from multiprocessing.connection import Client
+from multiprocessing.connection import Client, Listener
 from std_msgs.msg import String
 
-# Connection to the flask server.server_connection = None
+# Flask server process.
+server_script_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "flask_server.py")
+server_process = None
 
+# Implement a full duplex connection with the flask server.
+sender_address = ('localhost', 7000)
+receiver_address = ('localhost', 7001)
+
+server_sender = None
+server_receiver_listener = None
+server_receiver = None
+
+
+def initialize_server_process():
+    server_process = sp.Popen(["/usr/bin/python3", server_script_path])
+
+
+def initialize_server_connection():
+    # Give the subprocess some time to initialize its connection listener.
+    time.sleep(1)
+    # Create sender.
+    server_sender = Client(sender_address)
+
+    # Create a listener.
+    server_receiver_listener = Listener(receiver_address)
+
+    # Tell the server process that it can now start a sender client.
+    # NOTE: the server should wait a little until the next line is
+    # executed, otherwise the operation will fail.
+    server_sender.send("CreateSender")
+
+    server_receiver = server_receiver_listener.accept()
+
+
+def cleanup():
+    server_sender.send("Close")
+    # Give the server time to close itself.
+    time.sleep(1)
+    server_sender.close()
+    server_process.kill()
+
+    
 def main():
     rospy.init_node('hri', anonymous=True)
 
-    # Create a new process to execute the flask server.
-    server_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "flask_server.py")
-    p = sp.Popen(["/usr/bin/python3", server_path])
-
-    address = ('localhost', 7000)
-    # Give the subprocess some time to initialize its connection listener.
-    time.sleep(1)
-    server_connection = Client(address)
-
+    initialize_server_process()
+    initialize_server_connection()
+    
     rospy.spin()
 
-    # Cleanup the flask server process.
-    server_connection.close()
-    p.kill()
-    rospy.loginfo("Killed web server")
+    cleanup()
     
 
 if __name__ == '__main__':
