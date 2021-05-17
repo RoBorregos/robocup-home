2e15d103aa94e9511e262e20c6cc8e22d0c29c8f
Paul Vazquez <paulvazbad>
2021-05-17 11:48:43 -0700

    Added navigation challenge script

--- catkin_home/src/main_engine/challenges/TMR2021/navigation_challenge.py
+++ catkin_home/src/main_engine/challenges/TMR2021/navigation_challenge.py
@@ -12,17 +12,11 @@ def main():
     roslaunch.configure_logging(uuid)
     print(os.getcwd())
     # TODO: Launch file of navigation?
-    package = 'main_engine'
-    executable = 'action_manager.py'
-    node = roslaunch.core.Node(package, executable, executable)
-
-    launch = roslaunch.scriptapi.ROSLaunch()
+    launch = roslaunch.parent.ROSLaunchParent(uuid, ["./src/main_engine/launch/main_engine.launch"])
     launch.start()
 
-    process = launch.launch(node)
-
     # Publish todo with argv[1]
-    '''
+
     if(len(sys.argv) > 1):
         print(sys.argv[1])
         action_request = action_selector_cmd()
@@ -32,11 +26,12 @@ def main():
         rospy.loginfo(action_request)
         rospy.init_node("navigation_challenge_node")
         action_requested_pub = rospy.Publisher('action_requested',
-                          action_selector_cmd, queue_size=10)
+                          action_selector_cmd, queue_size=10, latch=True)
         action_requested_pub.publish(action_request)
+        rospy.spin()
     else:
         raise Exception("No target destination provided for go_to")
-        '''
+
 
 if __name__=="__main__":
     main()
