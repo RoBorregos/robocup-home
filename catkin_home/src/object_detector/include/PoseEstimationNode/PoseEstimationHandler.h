a2da4413e763f956a68430a723a6960c2bf32db1
RicardoChapaRomero <ricardochaparomero@gmail.com>
2021-10-23 23:55:41 -0500

    Add pose estimation handler class with OpenCv

--- catkin_home/src/object_detector/include/PoseEstimationNode/PoseEstimationHandler.h
+++ catkin_home/src/object_detector/include/PoseEstimationNode/PoseEstimationHandler.h
@@ -3,20 +3,55 @@
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "sensor_msgs/CompressedImage.h"
+
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+static const std::string OPENCV_WINDOW = "Image window";
 
 class PoseEstimationHandler {
 	public:
-		PoseEstimationHandler(ros::NodeHandle& pose_estimation_node) {
-			intel_cam_sub_ = pose_estimation_node.subscribe(
-				"/camera/color/image_raw/compressed", 1000, PoseEstimationCallback);
+		PoseEstimationHandler(ros::NodeHandle& pose_estimation_node) : it_(pose_estimation_node) {
+			ROS_INFO("Subscribing to IntelCamera image node...");
+			intel_cam_sub_ = it_.subscribe(
+				"/camera/color/image_raw", 1, &PoseEstimationHandler::imageCompressedCallback, this);
+
+			image_pub_ = it_.advertise("/pose_estimation_node/output_video", 1);
+			cv::namedWindow(OPENCV_WINDOW);
+			ROS_INFO("Subscriber initialized!");
+		}
+
+		~PoseEstimationHandler() {
+			cv::destroyWindow(OPENCV_WINDOW);
 		}
 
 	private:
-		static void PoseEstimationCallback(const std_msgs::String::ConstPtr& msg) {
-			ROS_INFO("I heard: [%s]", msg->data.c_str());
+		void imageCompressedCallback(const sensor_msgs::ImageConstPtr& event) {
+			// TODO: Process ROS Image to get object pose estimation
+			displayCVImage(event);
 		}
 
-		ros::Subscriber intel_cam_sub_;
+		void displayCVImage(const sensor_msgs::ImageConstPtr& event) {
+			cv_bridge::CvImagePtr cv_ptr;
+			try {
+				cv_ptr = cv_bridge::toCvCopy(event, sensor_msgs::image_encodings::BGR8);
+			} catch (cv_bridge::Exception& e) {
+				ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+			}
+
+			cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+			cv::waitKey(3);
+			image_pub_.publish(cv_ptr->toImageMsg());
+		}
+
+		image_transport::ImageTransport it_;
+  	image_transport::Subscriber intel_cam_sub_;
+  	image_transport::Publisher image_pub_;
 };
 
 #endif
--- catkin_home/src/object_detector/src/pose_estimation/PoseEstimation.cpp
+++ catkin_home/src/object_detector/src/pose_estimation/PoseEstimation.cpp
@@ -1,9 +1,11 @@
 #include <PoseEstimationNode/PoseEstimationHandler.h>
-
+#include "ros/ros.h"
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "pose_estimation_node");
 	ros::NodeHandle n;
+
+	ROS_INFO("Initializing Pose Estimation Node");
 	PoseEstimationHandler handler(n);
-	ros::spin(); 
+	ros::spin();
 	return 0;
 }
