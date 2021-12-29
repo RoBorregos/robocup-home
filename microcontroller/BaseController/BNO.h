0f7ab0a2a165768780909208f97953f0ad2dac4d
josecisneros001 <joseacisnerosm@gmail.com>
2021-12-28 23:27:32 -0600

    change to BaseController folder

---
+++ microcontroller/BaseController/BNO.h
@@ -0,0 +1,40 @@
+// This class has the declaration, initialization and usage function of the BNO.
+#ifndef BNO_h
+#define BNO_h
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+class BNO {
+
+  public:
+    //////////////////////////////////Constructor//////////////////////////////////////
+    BNO();
+
+
+    //////////////////////////////////Calibration//////////////////////////////////////
+    // Function that return a value between '0' (uncalibrated data) and '3' (fully calibrated).
+    uint8_t orientationStatus();
+
+
+    //////////////////////////////////Get Functions//////////////////////////////////////
+    // Returns the angle in X axis considering bno_set_point_ and it is in a range of 0-360.
+    int getCurrentXAngle();
+    
+    // Returns the angle in X axis.
+    double getAngleX();
+    
+    // Returns the angle in Y axis.
+    double getAngleY();
+    
+    // Returns the angle in Z axis.
+    double getAngleZ();
+
+    
+  private:
+    Adafruit_BNO055 bno_;
+    int bno_set_point_ = 0;
+};
+#endif
