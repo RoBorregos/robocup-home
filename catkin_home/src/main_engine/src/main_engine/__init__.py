e3097f5496d10df3a1a852ba3b83697b2000193c
Kevin Vega <vegakevinrdz@gmail.com>
2023-03-22 17:19:47 -0600

    Start davinci context

--- catkin_home/src/main_engine/src/main_engine/__init__.py
+++ catkin_home/src/main_engine/src/main_engine/__init__.py
@@ -1,16 +1,1 @@
 #!/usr/bin/env python3
-
-import os
-import openai
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="Hello",
-  temperature=0.7,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
