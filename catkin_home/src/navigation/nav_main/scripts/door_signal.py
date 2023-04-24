66706de61a4b44ed8feb2ece1b06e8b53f43959e
BryanMqz <A01562119@tec.mx>
2023-04-24 08:18:17 -0600

    Door Signal

--- catkin_home/src/navigation/nav_main/scripts/door_signal.py
+++ catkin_home/src/navigation/nav_main/scripts/door_signal.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import String
@@ -17,8 +16,8 @@ class DoorDetector:
 
     def lidar_callback(self, msg):
         # Filter the LiDAR data to only include points near the door
-        door_angle = 1.6  # Replace with the angle of the door in the LiDAR data
-        door_distance = 0.4  # Replace with the distance of the door in the LiDAR data
+        door_angle = 1.6  # Replace with the angle of the door in the LiDAR data (rad) 1.6 is straight of our robot
+        door_distance = 0.4  # Replace with the distance of the door in the LiDAR data (m)
         door_idx = int(door_angle / msg.angle_increment)
         min_idx = max(0, door_idx - 10)
         max_idx = min(len(msg.ranges), door_idx + 10)
@@ -26,13 +25,11 @@ class DoorDetector:
 
         # Calculate the median distance to the door
         door_distance = sorted(door_ranges)[len(door_ranges)//2]
-        rospy.loginfo("Door: %f", door_distance)
+        #rospy.loginfo("Door: %f", door_distance)
         # Check if the distance has changed significantly since the last scan
         if self.last_distance is not None and abs(door_distance - self.last_distance) > 0.12:
             self.door_opened = True
-
         self.last_distance = door_distance
-        # rospy.loginfo("Door: %f, Last: %f", door_distance, self.last_distance)
 
     def run(self):
         rate = rospy.Rate(10)  # 10 Hz
@@ -42,7 +39,6 @@ class DoorDetector:
                 cmd_msg = String()
                 cmd_msg.data = "start_moving"
                 self.cmd_pub.publish(cmd_msg)
-
             rate.sleep()
 
 if __name__ == '__main__':
