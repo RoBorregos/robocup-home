36a55f2f6a893c5e65926c250c77b71ec730cf4e
EmilianoHFlores <A00833160@tec.mx>
2023-11-08 21:22:56 -0600

    Clustering placing position finder

---
+++ catkin_home/src/manipulation/object_detector/src/Clustering_Server.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# A ROS service that receives a pointcloud and runs k-means clustering on it.
+# Returns the center x, y of the largest cluster.
+
+import rospy
+import numpy as np
+import sensor_msgs.point_cloud2 as pc2
+from sklearn.cluster import KMeans
+from object_detector.srv import Clustering
+import matplotlib.pyplot as plt
+import cv2
+
+class Clustering_Service:
+    def __init__(self):
+        # Create service 
+        self.service = rospy.Service('Clustering', Clustering, self.handle_clustering)
+        rospy.loginfo("Clustering service ready")
+        rospy.spin()
+    
+    def handle_clustering(self, req):
+        # Convert pointcloud to numpy array, ignore z axis
+        point_cloud = req.pointcloud
+        print(point_cloud)
+        point_cloud_array = []
+        for p in pc2.read_points(point_cloud, field_names = ("x", "y", "z"), skip_nans=True):
+            # append point to array
+            if not np.isnan(p[0]) and not np.isnan(p[1]):
+                point_cloud_array.append([p[0], p[1]])
+        point_cloud_array = np.array(point_cloud_array)
+        print(point_cloud_array)
+
+        # Run k-means clustering
+        n_clusters = req.n_clusters
+        rospy.loginfo("Running k-means clustering with %d clusters", n_clusters)
+        kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(point_cloud_array)
+        rospy.loginfo("Clustering complete, computing largest cluster")
+
+        # Find largest cluster
+        # draw the centroids of the biggest cluster as a green circle
+        # get the biggest cluster
+        biggest_cluster = 0
+        biggest_cluster_size = 0
+        for i in range(n_clusters):
+            if len(kmeans.labels_[kmeans.labels_ == i]) > biggest_cluster_size:
+                biggest_cluster = i
+                biggest_cluster_size = len(kmeans.labels_[kmeans.labels_ == i])
+        # draw the centroid of the biggest cluster
+        centroid = kmeans.cluster_centers_[biggest_cluster]
+        rospy.loginfo("Largest cluster found, centroid: %s", centroid)
+
+        # Draw clusters with biggest cluster centroid drawn in green, save as matplotlib image
+        plt.scatter(point_cloud_array[:,0], point_cloud_array[:,1], c=kmeans.labels_, cmap='rainbow')
+        plt.scatter(centroid[0], centroid[1], c='green', s=1000, alpha=0.8)
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('Clusters')
+
+        # save as file
+        rospy.loginfo("Saving clusters as image")
+        plt.savefig('/home/emilianh/clusters.png')
+
+        rospy.loginfo("Returning centroid")
+
+        # Return centroid
+        return centroid[0], centroid[1], True
+
+
+
+def main():
+    rospy.init_node('Clustering_Service', anonymous=True)
+    Clustering_Service()
+
+if __name__ == '__main__':
+    main()
