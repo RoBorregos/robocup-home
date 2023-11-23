fbf8882c29a2d88a29071278b0324c45378db7eb
mariinaVillanueva <marinavillanuevape@gmail.com>
2023-11-23 14:56:04 -0600

    dataframe with embeddings creation using Panda and OpenAI

---
+++ catkin_home/src/speech/dataFrames/create_dataset_embeddings.py
@@ -0,0 +1,409 @@
+import openai
+from openai.embeddings_utils import get_embedding, cosine_similarity
+import pandas as pd
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def create_actions():
+    actions = [{
+        "verb": "go"
+    }, {
+        "verb": "find"
+    }, {
+        "verb": "grab"
+    }, {
+        "verb": "put"
+    }, {
+        "verb": "introduce"
+    }]
+   
+  
+    actions_df = pd.DataFrame(actions)
+    actions_df['text_embedding'] = actions_df.verb.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    #print(actions_df)
+
+    # To save the Data Frame:
+    actions_df.to_pickle("embeddings_actions.pkl")
+
+def create_locations():
+    locations = [{
+        "location": "bench"
+    }, {
+        "location": "hallway_table"
+    }, {
+        "location": "bar"
+    }, {
+        "location": "sidetable"
+    }, {
+        "location": "kitchen_table"
+    }, {
+        "location": "kitchen_counter"
+    }, {
+        "location": "stove"
+    }, {
+        "location": "bed"
+    }, {
+        "location": "dresser"
+    }, {
+        "location": "sideboard"
+    }, {
+        "location": "bookshelf"
+    }, {
+        "location": "pantry"
+    }, {
+        "location": "cabinet"
+    }, {
+        "location": "dinner_table"
+    }, {
+        "location": "couch_table"
+    }, {
+        "location": "outside"
+    }, {
+        "location": "trash_bin"
+    }, {
+        "location": "person"
+    }, {
+        "location": "user"
+    }]
+   
+    locations_df = pd.DataFrame(locations)
+    locations_df['text_embedding'] = locations_df.location.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    #print(locations_df)
+
+    # To save the Data Frame:
+    locations_df.to_pickle("embeddings_locations.pkl")
+
+def create_categories_locations():
+    categories_locations = [{
+        "category": "seating"
+    }, {
+        "category": "table"
+    }, {
+        "category": "utensil"
+    }, {
+        "category": "shelf"
+    }, {
+        "category": "appliance"
+    }]
+   
+    categories_locations_df = pd.DataFrame(categories_locations)
+    categories_locations_df['text_embedding'] = categories_locations_df.category.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(categories_locations_df)
+
+    # To save the Data Frame:
+    categories_locations_df.to_pickle("embeddings_categories_locations.pkl")
+
+def create_seatings():
+    seatings = [{
+        "location": "bench"
+    }, {
+        "location": "bed"
+    }]
+   
+    seatings_df = pd.DataFrame(seatings)
+    seatings_df['text_embedding'] = seatings_df.location.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(seatings_df)
+
+    # To save the Data Frame:
+    seatings_df.to_pickle("embeddings_seating.pkl")
+
+def create_tables():
+    tables = [{
+        "location": "kitchen_table"
+    }, {
+        "location": "couch_table"
+    }, {
+        "location": "hallway_table"
+    }, {
+        "location": "dinner_table"
+    }, {
+        "location": "sidetable"
+    }]
+   
+    tables_df = pd.DataFrame(tables)
+    tables_df['text_embedding'] = tables_df.location.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(tables_df)
+
+    # To save the Data Frame:
+    tables_df.to_pickle("embeddings_table.pkl")
+
+def create_utensils():
+    utensils = [{
+        "location": "bar"
+    }]
+   
+    utensils_df = pd.DataFrame(utensils)
+    utensils_df['text_embedding'] = utensils_df.location.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(utensils_df)
+
+    # To save the Data Frame:
+    utensils_df.to_pickle("embeddings_utensil.pkl")
+
+def create_shelves():
+    shelves = [{
+        "location": "kitchen_counter",
+    }, {
+        "location": "dresser",
+    }, {
+        "location": "sideboard",
+    }, {
+        "location": "bookshelf",
+    }, {
+        "location": "pantry",
+    }, {
+        "location": "cabinet",
+    }]
+   
+    shelves_df = pd.DataFrame(shelves)
+    shelves_df['text_embedding'] = shelves_df.location.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(shelves_df)
+
+    # To save the Data Frame:
+    shelves_df.to_pickle("embeddings_shelf.pkl")
+
+def create_appliance():
+    appliance = [{
+        "location": "stove"
+    }]
+   
+    appliance_df = pd.DataFrame(appliance)
+    appliance_df['text_embedding'] = appliance_df.location.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(appliance_df)
+
+    # To save the Data Frame:
+    appliance_df.to_pickle("embeddings_appliance.pkl")
+
+def create_items():
+    items = [{
+        "item": "deodorant"
+    }, {
+        "item": "tooth_paste"
+    }, {
+        "item": "cleaner"
+    }, {
+        "item": "fresh_discs"
+    }, {
+        "item": "sponge"
+    }, {
+        "item": "fanta"
+    }, {
+        "item": "beer_can"
+    }, {
+        "item": "coke"
+    }, {
+        "item": "seven_up"
+    }, {
+        "item": "energy_drink"
+    }, {
+        "item": "milk"
+    }, {
+        "item": "apple_juice"
+    }, {
+        "item": "tomato_sauce"
+    }, {
+        "item": "peanut_butter"
+    }, {
+        "item": "chicken_noodles"
+    }, {
+        "item": "mermelade"
+    }, {
+        "item": "veggie_noodles"
+    }, {
+        "item": "garlic_sauce"
+    }, {
+        "item": "chocolate"
+    },  {
+        "item": "cookies"
+    },{
+        "item": "orange_juice"
+    }, {
+        "item": "drops"
+    }, {
+        "item": "crackers"
+    }]
+   
+    items_df = pd.DataFrame(items)
+    items_df['text_embedding'] = items_df.item.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(items_df)
+
+    # To save the Data Frame:
+    items_df.to_pickle("embeddings_items.pkl")
+
+def create_categories_items():
+    categories_items = [{
+        "category": "cleaning_stuff"
+    }, {
+        "category": "drink"
+    }, {
+        "category": "food"
+    }, {
+        "category": "snack"
+    }]
+   
+    categories_items_df = pd.DataFrame(categories_items)
+    categories_items_df['text_embedding'] = categories_items_df.category.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(categories_items_df)
+
+    # To save the Data Frame:
+    categories_items_df.to_pickle("embeddings_categories_items.pkl")
+
+def create_cleaning_stuff():
+    cleaning_stuff = [{
+        "item": "deodorant",
+    }, {
+        "item": "tooth_paste"
+    }, {
+        "item": "cleaner"
+    }, {
+        "item": "fresh_discs"
+    }]
+   
+    cleaning_stuff_df = pd.DataFrame(cleaning_stuff)
+    cleaning_stuff_df['text_embedding'] = cleaning_stuff_df.item.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(cleaning_stuff_df)
+
+    # To save the Data Frame:
+    cleaning_stuff_df.to_pickle("embeddings_cleaning_stuff.pkl")
+
+def create_drinks():
+    drinks = [{ 
+        "item": "fanta"
+    }, {
+        "item": "beer_can"
+    }, {
+        "item": "coke"
+    }, {
+        "item": "seven_up"
+    }, {
+        "item": "energy_drink"
+    }, {
+        "item": "milk"
+    }, {
+        "item": "apple_juice"
+    }]
+   
+    drinks_df = pd.DataFrame(drinks)
+    drinks_df['text_embedding'] = drinks_df.item.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(drinks_df)
+
+    # To save the Data Frame:
+    drinks_df.to_pickle("embeddings_drink.pkl")
+
+def create_foods():
+    foods = [{
+        "item": "tomato_sauce"
+    }, {
+        "item": "peanut_butter"
+    }, {
+        "item": "chicken_noodles"
+    }, {
+        "item": "mermelade"
+    }, {
+        "item": "veggie_noodles"
+    }, {
+        "item": "garlic_sauce"
+    }]
+   
+    foods_df = pd.DataFrame(foods)
+    foods_df['text_embedding'] = foods_df.item.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(foods_df)
+
+    # To save the Data Frame:
+    foods_df.to_pickle("embeddings_food.pkl")
+
+def create_snacks():
+    snacks = [{
+        "item": "chocolate"
+    }, {
+        "item": "cookies"
+    }, {
+        "item": "orange_juice"
+    }, {
+        "item": "drops"
+    }, {
+        "item": "crackers"
+    }]
+   
+    snacks_df = pd.DataFrame(snacks)
+    snacks_df['text_embedding'] = snacks_df.item.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(snacks_df)
+
+    # To save the Data Frame:
+    snacks_df.to_pickle("embeddings_snack.pkl")
+
+def create_names():
+    names = [{
+        "name": "person"
+    }, {
+        "name": "Marina"
+    }, {
+        "name": "Ivan"
+    }, {
+        "name": "Alejandra"
+    }, {
+        "name": "Kevin"
+    }, {
+        "name": "Paquito"
+    }]
+   
+    names_df = pd.DataFrame(names)
+    names_df['text_embedding'] = names_df.name.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+
+    # To print the Data Frame
+    # print(names_df)
+
+    # To save the Data Frame:
+    names_df.to_pickle("embeddings_names.pkl")
+
+if __name__ == "__main__":
+    create_actions()
+
+    create_locations()
+    create_categories_locations()
+    create_seatings()
+    create_tables()
+    create_utensils()
+    create_shelves()
+    create_appliance()
+
+    create_items()
+    create_categories_items()
+    create_cleaning_stuff()
+    create_drinks()
+    create_foods()
+    create_snacks()
+
+    create_names()
+
+    print("Dataframes created succesfully")
