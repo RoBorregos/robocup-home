e1a30f64eed58849fc773d4569b068a4b95a3fd5
EmilianoHFlores <A00833160@tec.mx>
2023-11-26 12:59:15 -0600

    Main engine updated w/nav and man goals

---
+++ catkin_home/src/speech/scripts/demo_node_json_nav.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from threading import current_thread
+import rospy
+import actionlib
+
+import json
+
+from std_msgs.msg import String, Int32, Bool
+
+from arm_server.msg import MoveArmAction, MoveArmGoal
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+# from main_eng.srv import MainEng, MainEngResponse
+from speech.msg import command, list_of_commands
+import uuid
+
+COMMANDS_TOPIC = "/speech/processed_commands"
+MAN_SERV_TOPIC = "/manipulation_server"
+TRACKING_TOPIC = "/toggle_face_detection"
+MOVE_ARM_AS = '/move_arm_as'
+
+JSON_PATH = "maps/tdp_locations.json"
+
+OBJECTS_NAME= {
+    1 : 'COKE',
+    2 : 'APPLE',
+    3 : 'MUG',
+    4 : 'SOAP',
+    5 : 'BANANA',
+    6 : 'JAR'
+}
+
+OBJECTS_ID= {
+    'COKE' : 1,
+    'APPLE' : 2,
+    'MUG' : 3,
+    'SOAP' : 4,
+    'BANANA' : 5,
+    'JAR' : 6
+}
+
+class MainEngServer:
+    state = 0
+    STATE_ENUM = {
+        "IDLE": 0,
+        "RECEIVE_COMMANDS": 10,
+        "EXECUTING_COMMANDS": 20,
+        "STOPPING_SERVICES": 300,
+        "ERROR": 500,
+        "SHUTDOWN": 1000
+    }
+    COMMANDS_ENUM = {
+        "go": 0,
+        "stop": 10,
+        "grab": 20,
+        "put": 30,
+    }
+    def __init__(self):
+        self.node = rospy.init_node('main_eng_server')
+        # self.pub = rospy.Service('main_eng', MainEng, self.main_eng_server)
+        self.sub = rospy.Subscriber(COMMANDS_TOPIC, list_of_commands, self.main_eng_command_subscriber) 
+        self.rate = rospy.Rate(10)
+        self.nav_json = json.load(open(JSON_PATH))
+        # print json
+        print(self.nav_json)
+        rospy.loginfo("Connecting to nav_server")
+        # self.nav_client = actionlib.SimpleActionClient('navServer', main_eng.msg.navServAction)
+        # self.nav_client.wait_for_server()
+        # self.manip_client = actionlib.SimpleActionClient(MAN_SERV_TOPIC, command) #CHANGE THIS
+        # self.manip_client.wait_for_server()
+
+        # for pose goals
+        self.move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        self.move_base_client.wait_for_server()
+
+        self.manipulation_pub = rospy.Publisher('manipulation/goal', String, queue_size=10)
+        
+        self.manipulation_sub = rospy.Subscriber('manipulation/response', String, self.manipulation_callback)
+        
+        self.move_arm_client = actionlib.SimpleActionClient(MOVE_ARM_AS, MoveArmAction)
+        self.move_arm_client.wait_for_server()
+
+        self.reset_arm()
+
+        rospy.loginfo("MainEngServer started")
+        self.state = MainEngServer.STATE_ENUM["IDLE"]
+        #self.tracker_pub = rospy.Publisher(TRACKING_TOPIC, Bool, queue_size=10)
+        #self.tracker_pub.publish(True)
+        self.current_command = None
+        self.current_queue = []
+        self.current_thread = None
+
+        rospy.spin()
+
+    def reset_arm(self):
+        self.arm_goal = MoveArmGoal()
+        self.arm_goal.state = "hri"
+        self.arm_goal.speed = 0.3
+
+        self.move_arm_client.send_goal(self.arm_goal)
+        self.move_arm_client.wait_for_result()
+    
+    def manipulation_callback(self, data):
+        self.manipulation_status = data.data
+
+    # def main_eng_server(self, req):
+    #     message_input:str = req.message
+    #     if not message_input:
+    #         return MainEngResponse(MainEngServer.STATE_ENUM["ERROR"])
+        
+    #     if self.state != MainEngServer.STATE_ENUM["IDLE"]:
+    #         rospy.logwarn("Received command while not in IDLE state")
+    #         # if stoping services, wait for services to stop then override command
+    #     else:
+    #         self.state = MainEngServer.STATE_ENUM["RECEIVE_COMMANDS"]
+    #         return self.main_eng_command_manager(message_input)
+
+    def cancel_current_command(self):
+        rospy.loginfo("Cancelling current command")
+        if self.current_command == "go":
+            rospy.loginfo("Cancelling go")
+            # self.nav_client.cancel_goal()
+        elif self.current_command == "grab":
+            rospy.loginfo("Cancelling grab")
+            # self.manip_client.cancel_goal()
+            pass
+        elif self.current_command == "put":
+            rospy.loginfo("Cancelling put")
+            # self.manip_client.cancel_goal()
+            pass
+        elif self.current_command == "introduce":
+            rospy.loginfo("Cancelling introduce")
+            # self.cancel_introduce()
+            pass
+        else:
+            rospy.logwarn("Unknown command")
+
+    def main_eng_command_subscriber(self, commands_input):
+        rospy.loginfo("Received command")
+
+        if not commands_input:
+            return MainEngServer.STATE_ENUM["ERROR"]
+
+        # current_thread = uuid.uuid1()
+        # self.current_thread = current_thread
+
+        if self.state != MainEngServer.STATE_ENUM["IDLE"]:
+            rospy.logwarn("Received command while not in IDLE state, cancelling current commands")
+            return MainEngServer.STATE_ENUM["ERROR"]
+            self.current_queue = []
+            self.cancel_current_command()
+            self.state = MainEngServer.STATE_ENUM["IDLE"]
+
+        current_thread = uuid.uuid1()
+        self.current_thread = current_thread
+        #self.tracker_pub.publish(False)
+        self.state = MainEngServer.STATE_ENUM["RECEIVE_COMMANDS"]
+        # self.current_queue = commands_input.data.split(", ")
+        self.current_queue = commands_input.commands
+        self.past_state = self.state
+        while len(self.current_queue) > 0 and self.current_thread == current_thread:
+            self.current_command = self.current_queue.pop(0)
+            self.past_state = self.main_eng_command_manager(self.current_command)
+            if self.past_state == MainEngServer.STATE_ENUM["ERROR"]:
+                rospy.logerror("Error in command manager")
+                self.state = MainEngServer.STATE_ENUM["ERROR"]
+                self.cancel_current_command()
+                self.current_queue = []
+                self.state = MainEngServer.STATE_ENUM["IDLE"]
+                break
+        self.reset_arm()
+        #self.tracker_pub.publish(True)
+        # self.main_eng_command_manager()
+
+
+    def main_eng_command_manager(self, command): 
+        # commands = message_input.split(", ")
+        # if not commands or len(commands) == 0:
+        #     return MainEngServer.STATE_ENUM["ERROR"]
+        # for command in commands:
+            # action, value = command.split(" ")
+        action = command.action
+        value = command.complements
+        rospy.loginfo("Action: " + action + " Value: ")
+        if(len(value) == 0):
+            value = None
+            self.state = MainEngServer.STATE_ENUM["ERROR"]
+            return MainEngServer.STATE_ENUM["ERROR"]
+        value = value[0]
+        rospy.loginfo("Action: " + action + " Value: " + value)
+        if action == "go":
+            self.state = MainEngServer.STATE_ENUM["EXECUTING_COMMANDS"]
+            self.go_to_location(value)
+        elif action == "stop":
+            self.state = MainEngServer.STATE_ENUM["STOPPING_SERVICES"]
+            self.stop()
+        elif action == "grab":
+            self.state = MainEngServer.STATE_ENUM["EXECUTING_COMMANDS"]
+            self.grab(value)
+        elif action == "put":
+            self.state = MainEngServer.STATE_ENUM["EXECUTING_COMMANDS"]
+            self.go(value)
+            self.put(value)
+        elif action == "introduce":
+            self.state = MainEngServer.STATE_ENUM["EXECUTING_COMMANDS"]
+            self.intro()
+        else:
+            return MainEngServer.STATE_ENUM["ERROR"]
+    
+        self.state = MainEngServer.STATE_ENUM["IDLE"]
+
+        return MainEngServer.STATE_ENUM["IDLE"]
+
+    def go(self, value):
+        rospy.loginfo("Going to " + value)
+        # self.nav_client.send_goal_and_wait(target_location = value)
+        rospy.loginfo("Arrived at " + value)
+
+    def go_to_location(self, location):
+        for key in self.nav_json:
+            if key.lower() == location.lower():
+                rospy.logwarn(f"Sending location: {location}")
+                goal = MoveBaseGoal()
+                goal.target_pose.header.frame_id = "map"
+                goal.target_pose.header.stamp = rospy.Time.now()
+                goal.target_pose.pose.position.x = self.nav_json[key][0]
+                goal.target_pose.pose.position.y = self.nav_json[key][1]
+                goal.target_pose.pose.position.z = self.nav_json[key][2]
+                goal.target_pose.pose.orientation.x = self.nav_json[key][3]
+                goal.target_pose.pose.orientation.y = self.nav_json[key][4]
+                goal.target_pose.pose.orientation.z = self.nav_json[key][5]
+                goal.target_pose.pose.orientation.w = self.nav_json[key][6]
+
+                self.move_base_client.send_goal(goal)
+                self.move_base_client.wait_for_result()
+                rospy.logdebug(f"Result: {self.move_base_client.get_result()}")
+                return True
+        rospy.logwarn(f"Location not found: {location}")
+        return False
+
+
+    def stop(self):
+        rospy.loginfo("Stopping")
+
+    def grab(self, value):
+        MODEL_DICT = {
+            "Apple": "manzanas",
+            "Cookies": "galletas",
+            "Pringles": "pringles",
+            "Coke": "cocacola",
+            "Cereal": "zucaritas",
+            "soap": "zote"
+        }
+        value = MODEL_DICT[value]
+        self.manipulation_pub.publish(value)
+        rospy.loginfo("Waiting for grab")
+        status = self.manipulation_sub = rospy.Subscriber('manipulation/response', String, self.manipulation_callback)
+        rospy.loginfo("Grabbing " + value)
+        if status == "True":
+            rospy.loginfo("Grabbed " + value)
+            # self.say(promts["grasp_success"])
+            return True
+        elif status == "False":
+            rospy.loginfo("Failed to grab " + value)
+            # self.say(promts["grasp_fail"])
+            return False
+
+    def put(self, value):
+        rospy.loginfo("Putting " + value)
+        if(value == "user"):
+            self.cancel_current_command()
+            self.current_queue = []
+            self.state = MainEngServer.STATE_ENUM["IDLE"]
+
+    def intro(self):
+        rospy.loginfo("Introducing")
+
+if __name__ == "__main__":
+    try:
+        MainEngServer()
+    except rospy.ROSInterruptException as e:
+        rospy.logerr(e)
+        pass
