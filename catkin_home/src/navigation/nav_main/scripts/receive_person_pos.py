98abf890bbac3378541601bc3a676f6098131488
Diego Hernandez <a00834015@tec.mx>
2024-03-25 19:11:34 -0600

    implement map modifier to set position of person as occupied

--- catkin_home/src/navigation/nav_main/scripts/receive_person_pos.py
+++ catkin_home/src/navigation/nav_main/scripts/receive_person_pos.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python3
-
-"""
-This node receives the position published by the human_position_publisher node 
-and transforms it to the map frame. It then publishes the position to the
-move_base action server to navigate to the person.
-
-This node runs on the jetson nano.
-"""
-
 import rospy
-from geometry_msgs.msg import PointStamped, Pose
-from tf2_geometry_msgs import PoseStamped
 import tf2_ros
+
+from geometry_msgs.msg import PointStamped
+from tf2_geometry_msgs import PoseStamped
 from std_msgs.msg import Bool
-import numpy as np
-import math
 from actionlib_msgs.msg import GoalStatusArray
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from nav_msgs.msg import OccupancyGrid
+
+import numpy as np
+import math
 import actionlib
+
 
 class HumanPositionGetter:
     """
@@ -26,14 +21,32 @@ class HumanPositionGetter:
 
     def __init__(self) -> None:
         self.rate = rospy.Rate(10)
-        self.follow_person = True
+        self.follow_person = False
 
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
-        # self.tf_listener.wait_for_transform("map", "base_footprint", rospy.Time(), rospy.Duration(1))
 
-        self.move_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        self.map_width = 0
+        self.map_height = 0
+        self.map_resolution = 0
+        self.map_origin_x = -1
+        self.map_origin_y = -1
+
+        self.current_person_pos = (-1, -1)
+        self.last_person_pos = (-1, -1)
+        self.map_data = []
+
+        self.modified_map_publisher = rospy.Publisher(
+            "/map", OccupancyGrid, queue_size=10
+        )
+        self.map_subscriber = rospy.Subscriber(
+            "/map_original", OccupancyGrid, self.map_callback
+        )
+
+        rospy.loginfo("Initializing")
+        self.move_client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
         self.move_client.wait_for_server()
+        rospy.loginfo("Initializing")
 
         self.pose_subscriber = rospy.Subscriber(
             "/person_pose_odom", PointStamped, self.pose_callback
@@ -42,15 +55,88 @@ class HumanPositionGetter:
             "/follow_person", Bool, self.follow_person_callback
         )
         self.odom_subscriber = rospy.Subscriber(
-            "/robot_pose", Pose, self.odom_callback
+            # Robot_pose when running AMCL and run slam_out_pose when running Hector_Slam
+            "/slam_out_pose",
+            PoseStamped,
+            self.odom_callback,
         )
         self.goal_sub = rospy.Subscriber(
             "/move_base/status", GoalStatusArray, self.goal_callback
         )
+
         self.person_pose_map_pub = rospy.Publisher(
             "/person_pose_map", PoseStamped, queue_size=10
         )
 
+        rospy.loginfo("Topic Ready")
+
+    def get_area(self, width, center_row, center_col):
+        print(
+            center_row - width // 2,
+            center_row + width // 2,
+            center_col - width // 2,
+            center_col + width // 2,
+        )
+        return (
+            int(max(center_row - width // 2, 0)),
+            int(min(center_row + width // 2, self.map_height)),
+            int(max(center_col - width // 2, 0)),
+            int(min(center_col + width // 2, self.map_width)),
+        )
+
+    def set_area_to_value(
+        self, map: OccupancyGrid, value, start_row, end_row, start_col, end_col
+    ):
+        print(start_row, end_row, start_col, end_col)
+        for row in range(start_row, end_row):
+            for col in range(start_col, end_col):
+                self.modify_map(map, row, col, value)
+
+    def modify_map(self, map: OccupancyGrid, row, col, value):
+        index = self.map_width * row + col
+        
+        self.map_data[index] = value
+
+    def publish_map(self, map: OccupancyGrid):
+        map.data = tuple(self.map_data)
+        self.modified_map_publisher.publish(map)
+
+    def map_callback(self, map: OccupancyGrid):
+        self.map_data = list(map.data)
+        self.map_width = map.info.width
+        self.map_height = map.info.height
+        self.map_resolution = map.info.resolution
+        self.map_origin_x = map.info.origin.position.x
+        self.map_origin_y = map.info.origin.position.y
+
+        # if not self.follow_person:
+        #     self.publish_map(map)
+        #     return
+
+        if self.last_person_pos != (-1, -1):
+            print("removing last person pos")
+            self.set_area_to_value(map, -1, *self.get_area(15, *self.last_person_pos))
+
+        self.last_person_pos = (-1, -1)
+
+        if self.current_person_pos != (-1, -1):
+            print("adding current person pos")
+            self.set_area_to_value(
+                map, 100, *self.get_area(15, *self.current_person_pos)
+            )
+
+        self.publish_map(map)
+
+    def get_position_cell(self, x, y):
+        y -= self.map_origin_y
+        x -= self.map_origin_x
+
+        row = y // self.map_resolution
+        col = x // self.map_resolution
+
+        print(f"get position cell: {row, col}")
+
+        return row, col
 
     def get_quaternion_from_euler(self, roll, pitch, yaw):
         """
@@ -81,11 +167,20 @@ class HumanPositionGetter:
 
     def follow_person_callback(self, follow_person: Bool):
         print("follow person")
-        self.follow_person = follow_person
+        print(f"Received: {follow_person}")
+        self.follow_person = follow_person.data
 
-    def odom_callback(self, robot_pose: Pose):
+    def odom_callback(self, robot_pose: PoseStamped):
+        robot_pose = robot_pose.pose
         self.robot_position = np.array(
             [robot_pose.position.x, robot_pose.position.y, robot_pose.position.z]
+        )
+        self.robot_orientation = np.array(
+            [
+                robot_pose.orientation.x,
+                robot_pose.orientation.y,
+                robot_pose.orientation.z,
+            ]
         )
 
     # For thread on sending new goal once it completes the last one
@@ -93,7 +188,9 @@ class HumanPositionGetter:
         self.goal_status = data.status_list
 
     def pose_callback(self, person_pose_odom: PointStamped):
-        print(f"pose callback {person_pose_odom.point.x}, {person_pose_odom.point.y}, {person_pose_odom.point.z}")
+        print(
+            f"pose callback {person_pose_odom.point.x}, {person_pose_odom.point.y}, {person_pose_odom.point.z}"
+        )
         if not self.follow_person:
             return
 
@@ -104,6 +201,17 @@ class HumanPositionGetter:
         pose.pose.position.x = person_pose_odom.point.x
         pose.pose.position.y = person_pose_odom.point.y
         pose.pose.position.z = 0
+
+        distance_to_person = np.sqrt(
+            (pose.pose.position.x) ** 2 + (pose.pose.position.y) ** 2
+        )
+        if distance_to_person < 0.5 or distance_to_person > 2.5:
+            print("Person too close or too far")
+            return
+
+        safe_distance_multiplier = 0.6
+        pose.pose.position.x *= safe_distance_multiplier
+        pose.pose.position.y *= safe_distance_multiplier
 
         try:
             self.tf_buffer.lookup_transform("map", "base_footprint", rospy.Time())
@@ -117,17 +225,15 @@ class HumanPositionGetter:
             # rospy.signal_shutdown("transformation not available")
 
         try:
-            person_pose_map = self.tf_buffer.transform(
-                pose, "map", rospy.Duration(1)
-            )
+            person_pose_map = self.tf_buffer.transform(pose, "map", rospy.Duration(1))
         except Exception as e:
             print(e)
             rospy.logerr("Failed to transform object pose from body_frame to map_frame")
             self.rate.sleep()
             return
             # rospy.signal_shutdown("Transformation failed")
-        
-        print(person_pose_map)
+
+        # print(person_pose_map)
 
         pose = PoseStamped()
         pose.header.stamp = rospy.Time.now()
@@ -137,11 +243,20 @@ class HumanPositionGetter:
         pose.pose.position.y = person_pose_map.pose.position.y
         pose.pose.position.z = 0
 
-        self.angle = math.atan(
-            (person_pose_map.pose.position.y - self.robot_position[1])
-            / (person_pose_map.pose.position.x - self.robot_position[0])
+        # update person positions for map modifications
+        self.last_person_pos = self.current_person_pos
+        self.current_person_pos = self.get_position_cell(
+            pose.pose.position.x, pose.pose.position.y
         )
+
+        self.angle = math.atan2(
+            (person_pose_map.pose.position.y - self.robot_position[1]),
+            (person_pose_map.pose.position.x - self.robot_position[0]),
+        )
+
         quat = self.get_quaternion_from_euler(0, 0, self.angle)
+
+        print(f"Robot angle: {self.robot_orientation[2]}    Goal Angle: {self.angle}")
 
         pose.pose.orientation.x = quat[0]
         pose.pose.orientation.y = quat[1]
@@ -150,20 +265,22 @@ class HumanPositionGetter:
 
         self.person_pose_map_pub.publish(pose)
 
-        # if (
-        #     self.goal_status == 3 or self.goal_status == -1 or self.goal_status == 4
-        # ) and (pose.pose.position.x - self.ned_origin[0]) <= 4:
-        # print("sending goal")
-        # goal = MoveBaseGoal()
-        # goal.target_pose = pose
-        # self.move_client.send_goal(goal)
+        if self.follow_person:
+            # if (
+            #     self.goal_status == 3 or self.goal_status == -1 or self.goal_status == 4
+            # ) and (pose.pose.position.x - self.ned_origin[0]) <= 4:
+            print("sending goal")
+            goal = MoveBaseGoal()
+            goal.target_pose = pose
+            self.move_client.send_goal(goal)
         # self.move_client.wait_for_result()
 
-            # Publish nav goal
+        # Publish nav goal
         # self.pub_follow.publish(pose)
 
-if __name__ == '__main__': 
-    rospy.init_node('human_pose_getter', anonymous=True)
-    
+
+if __name__ == "__main__":
+    rospy.init_node("human_pose_getter", anonymous=True)
+
     humanGetter = HumanPositionGetter()
     rospy.spin()
